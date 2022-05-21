951f3472f44ae4961c4f9a20e028ac024e349a57
Anandakrishnan66 <ananda2000krishnan@gmail.com>
2022-05-21 16:16:08 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+//import './App.css';
+import axios from 'axios'
+import {useState} from 'react'
 function App() {
+  const [state,setState]=useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <h1>Hello</h1>
+   <button onClick={()=>{
+     axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+       console.log(response.data)
+       setState(response.data)
+     })
+   }}>
+     click
+   </button>
+   {
+     state.map((obj,index)=>{
+       return(
+         <div>
+              <h1>{index}</h1>
+              <h1>{obj.title}</h1>
+              <h4>{obj.body}</h4>
+              <h1>{obj.userId}</h1>
+           </div>
+       )
+     })
+   }
+  </div>
+  )
 }
 
 export default App;
